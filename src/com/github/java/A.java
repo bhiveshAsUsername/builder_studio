d64e3bed269a533945ad14c685670fe55776ee57
bhivesh <bhivesh.yadav@builder.ai>
2022-06-08 20:21:46 +0530

    master commit

--- src/com/github/java/A.java
+++ src/com/github/java/A.java
@@ -5,5 +5,7 @@ public static void main(String ar[]) {
 	System.out.println("Bhivesh Yadav in A");
 	System.out.println("Bhivesh Yadav in AB");
 	System.out.println("Bhivesh Yadav in ABC");
+	System.out.println("added something new");
+	System.out.println("added on master stage work");
 }
 }
